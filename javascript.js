8ed9857b3847a45937fad447e7aac4755f5d360c
Roahge <125326895+Roahge@users.noreply.github.com>
2023-03-17 20:01:52 -0400

    Completed etch-a-sketch(Hopefully)

--- javascript.js
+++ javascript.js
@@ -1,11 +1,47 @@
 const gridContainer = document.querySelector('.grid-container');
+const button = document.querySelector('button');
 
-const gridCreation = () => {
-for(let i = 1; i < 257; i++) {
-    const div = document.createElement("div");
-    gridContainer.appendChild(div);
-    div.style.border = '1px solid red';
-    gridContainer.style.border = '1px solid red';
+
+
+
+
+const gridCreation = (size) => {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let gridSize = (size * size);
+
+
+    if(size > 100 || size) {size = 100;}
+        for(let i = 0; i < gridSize; i++) {
+            const div = document.createElement("div");
+            div.classList.add("grid");
+            div.onmouseover = function(){div.style.background = "black"};
+            gridContainer.appendChild(div);
 }}
 
-gridCreation();
+function resizeGrid() {
+    const size = window.prompt('Choose a grid size number between 0 and 100');
+
+    if (isNaN(size) || size < 0 || size > 100) {
+        window.alert('The number you entered is not an acceptable value, please choose between 0 and 100');
+    }
+    {
+        deleteGrid();
+        gridCreation(size);
+    }
+}
+
+function deleteGrid() {
+    const items = document.getElementsByClassName('.grid');
+    const total = items.length;
+
+    for(let i = 0; i <total; i++) {
+        items[0].parentNode.removeChild(items[0]);
+    }
+}
+button.addEventListener('click', () => {
+    resizeGrid();
+})
+
+gridCreation(16);
